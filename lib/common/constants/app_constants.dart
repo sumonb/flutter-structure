cf636201be8693d92fe677a59517a73beaee4f5d
Sumon Barua <sumon.barua@lbhpartners.com>
2019-07-29 08:04:38 +1000

    First Commit

---
+++ lib/common/constants/app_constants.dart
@@ -0,0 +1,3 @@
+class AppConstants {
+  static const APPLICATION_TESTING = "";
+}
---
+++ lib/common/constants/image_const.dart
@@ -0,0 +1,5 @@
+const IMAGE_PATH = "assets/images";
+
+class PageImage {
+  static const FLUTTER_OPEN = "$IMAGE_PATH/launcher.png";
+}
